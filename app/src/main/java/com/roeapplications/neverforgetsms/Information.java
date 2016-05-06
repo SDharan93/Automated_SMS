c53fa99a8be8a326a25617dc9be86835d026c99b
Shane Dharan <ShaneDharan@gmail.com>
2016-05-05 22:35:38 -0400

    Reformatting UI

--- app/src/main/java/com/roeapplications/neverforgetsms/Information.java
+++
@@ -1,8 +0,0 @@
-package com.roeapplications.neverforgetsms;
-
-/**
- * Created by shane on 2016-01-10.
- */
-public class Information {
-    String title;
-}
---
+++ app/src/main/java/com/roeapplications/neverforgetsms/MainActivity.java
@@ -0,0 +1,21 @@
+package com.roeapplications.neverforgetsms;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by shane on 2016-01-08.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
